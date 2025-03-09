833d512020e455ead7d0bf4bd9dd1ab7d5f7b5d8
Thien-Cao <thienbro1997@gmail.com>
2025-03-09 14:19:38 +0700

    first learning commit

---
+++ Demo/Demo.java
@@ -0,0 +1,40 @@
+package Demo;
+
+public class Demo {
+    public static void main(String[] args) {
+        // // Relational Operators
+        // // >, <, = , ! , ...
+
+        // double x = 6.6;
+        // double y = 6.6;
+        // boolean result = x == y;
+        // // Relational Operators
+
+        // int a = 8;
+        // int b = 7;
+
+        // int z= 5;
+        // int w= 6;
+
+        // boolean result2 = a > b && z < w;
+
+        // // ## -> ampersand
+        // // ! exclamation mark
+        // // pipe -> ||
+
+        // // Logical Operators (And | Or | Not)
+
+        // boolean resultInt = a > b;
+        // System.out.println(result);
+        // System.out.println(result2);
+
+        /// if else conditions
+        /// 
+        int x = 8;
+        if (x == 8) {
+            System.out.println("Hello");
+            return;
+        }
+        System.out.println("Bye");
+    }
+}
---
+++ Hello/Hello.java
@@ -0,0 +1,15 @@
+
+class Hello {
+    public static void main(String[] args) {
+
+        // Arthemetic Operator
+        int number = 7;
+        // ++number; -> pre increment -> increment and then fetch
+        // number++; -> post increment -> fetch the value and then increment
+        int result = number++;
+        // Arthemetic Operator
+        System.out.println(result);
+        System.out.println(number);
+
+    }
+}
